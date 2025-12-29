0c0bae1885e63c551efb626009ce587d2b08ec17
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2025-12-29 13:26:21 +0530

    fix: use node to run prisma directly to fix permission denied

--- server/render-build.sh
+++ server/render-build.sh
@@ -21,6 +21,6 @@ echo "Debug: Listing .bin directory..."
 ls -la node_modules/.bin
 
 echo "Generating Prisma client..."
-npx prisma generate
+node node_modules/prisma/build/index.js generate
 
 echo "Build done!"
